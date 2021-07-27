e15da3a768edddcb53dc134df1184987b1947efb
luchiyu <luchiyu8@gmail.com>
2021-07-27 21:50:02 +1200

    End of Section

---
+++ Domain/Activity.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Domain
+{
+    public class Activity
+    {
+        public Guid ID { get; set; }
+        public String Title { get; set; }
+        public DateTime Date { get; set; }
+        public String Description { get; set; }
+        public String Category { get; set; }
+        public String City { get; set; }
+        public String Venue { get; set; }
+    }
+}
